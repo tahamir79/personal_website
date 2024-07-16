15f208b536067a5e980c26af4ab8408d1acc1dce
Taha M <s.tahmir79@gmail.com>
2024-07-15 21:42:07 -0500

    hello

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,11 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import { getSortedPostsData } from '../lib/posts'
-import Date from '../components/date'
-
-
-// Your code here...
-
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+// pages/index.js
+const Home = () => {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hi, I'm John Doe. Welcome to my personal website where I share my thoughts, projects, and hobbies.</p>
-        <p>
-          Check out my <Link href="/about">About Me</Link> page to learn more about me.
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`} legacyBehavior>
-                <a className={utilStyles.blogLink}>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
-}
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This isn&apos;t working</p>
+    </div>
+  );
+};
+
+export default Home;
